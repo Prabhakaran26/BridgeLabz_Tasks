cb054f296135c4d87ca7c961d622f17152ef6db1
Prabhakaran26 <prabhakaranjayapal26@gmail.com>
2026-06-11 21:10:12 +0530

    Add - [Prabhakaran] : Added Use Case - 4

--- src/main/java/org/example/day19/UserRegistration.java
+++ src/main/java/org/example/day19/UserRegistration.java
@@ -4,6 +4,8 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     private static final String NAME_REGEX = "^[A-Z][a-zA-Z]{2,}$";
     private static final String EMAIL_REGEX = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$";
+    private static final String MOBILE_REGEX = "^[0-9]{2}\\s[0-9]{10}$";
+
 
     public boolean validateFirstName(String firstName) {
         return Pattern.matches(NAME_REGEX, firstName);
@@ -15,6 +17,10 @@ public class UserRegistration {
 
     public boolean validateEmail(String email) {
         return Pattern.matches(EMAIL_REGEX, email);
+    }
+
+    public boolean validateMobile(String mobile) {
+        return Pattern.matches(MOBILE_REGEX, mobile);
     }
 
     public static void main(String[] args){
