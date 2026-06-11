cc6a4c501ca4c71d3909c25a7fa92a1115f7e4a3
Prabhakaran26 <prabhakaranjayapal26@gmail.com>
2026-06-11 21:07:36 +0530

    Add - [Prabhakaran] : Added Use Case - 3

--- src/main/java/org/example/day19/UserRegistration.java
+++ src/main/java/org/example/day19/UserRegistration.java
@@ -1,11 +1,9 @@
 package org.example.day19;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.regex.Pattern;
 
 public class UserRegistration {
     private static final String NAME_REGEX = "^[A-Z][a-zA-Z]{2,}$";
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$";
 
     public boolean validateFirstName(String firstName) {
         return Pattern.matches(NAME_REGEX, firstName);
@@ -15,8 +13,12 @@ public class UserRegistration {
         return Pattern.matches(NAME_REGEX, lastName);
     }
 
+    public boolean validateEmail(String email) {
+        return Pattern.matches(EMAIL_REGEX, email);
+    }
+
     public static void main(String[] args){
         UserRegistration ur = new UserRegistration();
-        System.out.println(ur.validateFirstName("Prabhakaran"));
+        System.out.println(ur.validateEmail("prabhakran@gmail.com"));
     }
 }
