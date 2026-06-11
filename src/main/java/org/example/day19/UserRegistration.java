e40cde119996354c4b36dc3f70c1b45b906a104f
Prabhakaran26 <prabhakaranjayapal26@gmail.com>
2026-06-11 21:14:01 +0530

    Add - [Prabhakaran] : Added Use Case - 5,6,7,8

--- src/main/java/org/example/day19/UserRegistration.java
+++ src/main/java/org/example/day19/UserRegistration.java
@@ -5,6 +5,8 @@ public class UserRegistration {
     private static final String NAME_REGEX = "^[A-Z][a-zA-Z]{2,}$";
     private static final String EMAIL_REGEX = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$";
     private static final String MOBILE_REGEX = "^[0-9]{2}\\s[0-9]{10}$";
+    private static final String PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*[0-9])(?=[a-zA-Z0-9\\s]*[^a-zA-Z0-9\\s][a-zA-Z0-9\\s]*$).{8,}$";
+
 
 
     public boolean validateFirstName(String firstName) {
@@ -21,6 +23,10 @@ public class UserRegistration {
 
     public boolean validateMobile(String mobile) {
         return Pattern.matches(MOBILE_REGEX, mobile);
+    }
+
+    public boolean validatePassword(String password) {
+        return Pattern.matches(PASSWORD_REGEX, password);
     }
 
     public static void main(String[] args){
